af360eef75407e4aeeb99a174f6b13b3ab2044ec
amy <anmingyu@smartisan.com>
2018-08-23 14:55:10 +0800

    RemoveDuplicateIntegers1 and 2.
    Signed-off-by: amy <anmingyu@smartisan.com>

---
+++ JavaImpl/src/_0base/Base.java
@@ -0,0 +1,11 @@
+package _0base;
+
+public class Base {
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+
+    public static void println(Object o) {
+        System.out.println(o);
+    }
+}
