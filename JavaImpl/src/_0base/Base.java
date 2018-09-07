0eb013c9919715f5f364df083eb24fa361579022
amy <anmingyu@smartisan.com>
2018-09-07 12:54:57 +0800

    Next Permutation.
    Signed-off-by: amy <anmingyu@smartisan.com>

--- JavaImpl/src/_0base/Base.java
+++ JavaImpl/src/_0base/Base.java
@@ -1,6 +1,18 @@
 package _0base;
 
 public class Base {
+    public static void swap(int nums[], int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public static void reverse(int nums[], int begin, int end) {
+        while (begin < end) {
+            swap(nums, begin++, end--);
+        }
+    }
+
     public static void print(Object o) {
         System.out.print(o);
     }
