1ebf59bd77291e040ce9c5e026dc0d60b4e472b4
amy <anmingyu@smartisan.com>
2018-10-23 21:48:14 +0800

    Pascals SpiralMatrix.
    Signed-off-by: amy <anmingyu@smartisan.com>

---
+++ JavaImpl/src/_28simulation/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,16 @@
+package _28simulation;
+
+import java.util.List;
+
+import _00base.Base;
+
+public class SubstringWithConcatenationOfAllWords extends Base {
+
+    public static List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
