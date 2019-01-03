c9053c6e99ebbdebab2ff1eb80a3b0b391ac72d7
amy-home <349047303@qq.com>
2019-01-03 20:40:54 +0800

    [LeetCodeInJava] ReverseBit.
    Signed-off-by: amy-home <349047303@qq.com>

---
+++ LeetCodeInJava/src/bit/ReverseBit.java
@@ -0,0 +1,24 @@
+package bit;
+
+import base.Base;
+
+public class ReverseBit extends Base {
+
+    public int reverseBits(int n) {
+        if (n == 0) return 0;
+
+        int result = 0;
+        for (int i = 0; i < 32; ++i) {
+            result <<= 1;
+            if ((n & 1) == 1) {
+                ++result;
+            }
+            n >>= 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
