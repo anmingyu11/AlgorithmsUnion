099804896489239a5dfb59ed9acd86f45b6e2991
amy <anmingyu@smartisan.com>
2019-01-08 15:47:19 +0800

    [LeetCodeInJava] Some Comment in RectangleArea.
    Signed-off-by: amy <anmingyu@smartisan.com>

--- LeetCodeInJava/src/bit/RectangleArea.java
+++ LeetCodeInJava/src/bit/RectangleArea.java
@@ -14,10 +14,12 @@ public class RectangleArea extends Base {
         public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
             final int area = (D - B) * (C - A) + (H - F) * (G - E);
 
+            // 不相交
             if (C < E || G < A || D < F || H < B) {
                 return area;
             }
 
+            // 伪相交
             final int h = Math.max(Math.min(D, H) - Math.max(B, F), 0);
             final int w = Math.max(Math.min(C, G) - Math.max(A, E), 0);
             final int intersection = h * w;
