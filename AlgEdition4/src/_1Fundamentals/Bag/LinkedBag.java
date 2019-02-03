03bae6277d4b4c6f4c998f5846c81ba6503339f0
amy-home <349047303@qq.com>
2019-02-03 18:32:08 +0800

    [Alg4] Review Alg4 chap1.
    Signed-off-by: amy-home <349047303@qq.com>

---
+++ AlgEdition4/src/_1Fundamentals/Bag/LinkedBag.java
@@ -0,0 +1,129 @@
+package _1Fundamentals.Bag;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import base.stdlib.StdIn;
+import base.stdlib.StdOut;
+
+/**
+ * The {@code LinkedBag} class represents a bag (or multiset) of
+ * generic items. It supports insertion and iterating over the
+ * items in arbitrary order.
+ * <p>
+ * This implementation uses a singly linked list with a non-static nested class Node.
+ * See {@link Bag} for a version that uses a static nested class.
+ * The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
+ * take constant time. Iteration takes time proportional to the number of items.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+public class LinkedBag<Item> implements Iterable<Item> {
+    private Node first;    // beginning of bag
+    private int n;         // number of elements in bag
+
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+
+    /**
+     * Initializes an empty bag.
+     */
+    public LinkedBag() {
+        first = null;
+        n = 0;
+    }
+
+    /**
+     * Is this bag empty?
+     *
+     * @return true if this bag is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in this bag.
+     *
+     * @return the number of items in this bag
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Adds the item to this bag.
+     *
+     * @param item the item to add to this bag
+     */
+    public void add(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        ++n;
+    }
+
+
+    /**
+     * Returns an iterator that iterates over the items in the bag.
+     */
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    // an iterator over a linked list
+    private class ListIterator implements Iterator<Item> {
+        private Node current;
+
+        // creates a new iterator
+        public ListIterator() {
+            current = first;
+        }
+
+        // is there a next item in the iterator?
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        // this method is optional in Iterator interface
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        // returns the next item in the iterator (and advances the iterator)
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    /**
+     * Unit tests the {@code LinkedBag} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        LinkedBag<String> bag = new LinkedBag<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            bag.add(item);
+        }
+
+        StdOut.println("size of bag = " + bag.size());
+        for (String s : bag) {
+            StdOut.println(s);
+        }
+    }
+
+
+}
---
+++ AlgEdition4/src/_1Fundamentals/UnionFind/UFUtils.java
@@ -0,0 +1,50 @@
+package _1Fundamentals.UnionFind;
+
+import base.stdlib.In;
+
+public class UFUtils {
+    static final String TINY_UF = "https://algs4.cs.princeton.edu/15uf/tinyUF.txt";
+    static final String MEDIUM_UF = "https://algs4.cs.princeton.edu/15uf/mediumUF.txt";
+    static final String LARGE_UF = "https://algs4.cs.princeton.edu/15uf/largeUF.txt";
+
+    static class UFDataHolder {
+        int N = 0;
+        int[] ints = null;
+
+        public UFDataHolder(int n, int[] ints) {
+            N = n;
+            this.ints = ints;
+        }
+    }
+
+    static UFDataHolder getUFList(int type) {
+        switch (type) {
+            case 0: {
+                In in = new In(UFUtils.TINY_UF);
+                int N = in.readInt();
+                int[] ints = in.readAllInts();
+                return new UFDataHolder(N, ints);
+            }
+            case 1: {
+                In in = new In(UFUtils.MEDIUM_UF);
+                int N = in.readInt();
+                int[] ints = in.readAllInts();
+                return new UFDataHolder(N, ints);
+            }
+            case 2: {
+                In in = new In(UFUtils.LARGE_UF);
+                int N = in.readInt();
+                int[] ints = in.readAllInts();
+                return new UFDataHolder(N, ints);
+            }
+            case 3: {
+                int N = 10;
+                int[] ints = new int[]{1, 2, 1, 3, 1, 4, 1, 5, 1, 6};
+                return new UFDataHolder(N, ints);
+            }
+            default: {
+                throw new RuntimeException("Are u idoit or something?");
+            }
+        }
+    }
+}
