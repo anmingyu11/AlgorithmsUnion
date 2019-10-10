a52dcf835c50a53331fbb86a005aae239faad6aa
amy-home <349047303@qq.com>
2019-10-10 23:36:52 +0800

    [Leetcode] 947 Most Stones Removed With Same row or columns.
    Solution for encode col and row.
    
    Signed-off-by: amy-home <349047303@qq.com>

--- LeetCode/src/_java/_0947MostStonesRemovedWithSameRoworColumn_UNFMULT.java
+++ LeetCode/src/_java/_0947MostStonesRemovedWithSameRoworColumn_UNFMULT.java
@@ -1,9 +1,37 @@
 package _java;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import base.Base;
 
+/**
+ * On a 2D plane, we place stones at some integer coordinate points.
+ * Each coordinate point may have at most one stone.
+ * <p>
+ * Now, a move consists of removing a stone that shares a column or row
+ * with another stone on the grid.
+ * <p>
+ * What is the largest possible number of moves we can make?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+ * Output: 5
+ * Example 2:
+ * <p>
+ * Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+ * Output: 3
+ * Example 3:
+ * <p>
+ * Input: stones = [[0,0]]
+ * Output: 0
+ * <p>
+ * Note:
+ * <p>
+ * 1 <= stones.length <= 1000
+ * 0 <= stones[i][j] < 10000
+ */
 public class _0947MostStonesRemovedWithSameRoworColumn_UNFMULT extends Base {
 
     private abstract static class Solution {
@@ -112,6 +140,82 @@ public class _0947MostStonesRemovedWithSameRoworColumn_UNFMULT extends Base {
 
     }
 
+    /**
+     * Runtime: 6 ms, faster than 95.60% of Java online submissions for Most Stones Removed with Same Row or Column.
+     * Memory Usage: 43.7 MB, less than 50.00% of Java online submissions for Most Stones Removed with Same Row or Column.
+     * <p>
+     * Encoding Nodes
+     */
+    private static class Solution2 extends Solution {
+
+        private static class UF {
+
+            private int[] parent;
+            private int count;
+
+            public UF(int N) {
+                parent = new int[N];
+                count = N;
+                for (int i = 0; i < N; ++i) {
+                    parent[i] = i;
+                }
+            }
+
+            private int find(int p) {
+                while (p != parent[p]) {
+                    parent[p] = parent[parent[p]];
+                    p = parent[p];
+                }
+                return p;
+            }
+
+            private void union(int p, int q) {
+                int rootP = find(p);
+                int rootQ = find(q);
+                parent[rootQ] = rootP;
+            }
+
+        }
+
+        public int removeStones(int[][] stones) {
+            final int n = stones.length;
+            if (n < 1) {
+                return 0;
+            }
+            UF dsu = new UF(20000);
+            for (int[] stone : stones) {
+                // col connect to row
+                dsu.union(stone[0], 10000 + stone[1]);
+            }
+            HashSet<Integer> set = new HashSet<>();
+            for (int[] stone : stones) {
+                // row connect together
+                set.add(dsu.find(stone[0]));
+            }
+            return n - set.size();
+        }
+
+    }
+
+    private static class Solution3 extends Solution {
+
+        HashMap<Integer, Integer> nodes;
+
+        public int removeStones(int[][] stones) {
+            final int n = stones.length;
+            nodes = new HashMap<>();
+
+            return 0;
+        }
+
+        private void union(int p, int q) {
+        }
+
+        private int find(int p) {
+        }
+
+    }
+
     public static void main(String[] args) {
         int[][] stones1 =
                 {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
@@ -120,7 +224,7 @@ public class _0947MostStonesRemovedWithSameRoworColumn_UNFMULT extends Base {
         int[][] stones3 = {{0, 0}};
         int[][] stones4 = {{0, 1}, {1, 0}, {1, 1}};
 
-        Solution s = new Solution1();
+        Solution s = new Solution3();
 
         println(s.removeStones(stones1)); // 5
         println(s.removeStones(stones2)); // 3
