03acbf7757804c875cecc994530b12c65ee06063
安明宇 <anmingyu@dhgate.com>
2020-07-07 12:41:49 +0800

    [Leetcode] Description of 148 Sort List.
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- LeetCode/src/_java/_0148SortList.java
+++ LeetCode/src/_java/_0148SortList.java
@@ -4,6 +4,18 @@ import java.util.ArrayList;
 
 import base.BaseLinkedList;
 
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ *
+ * Example 1:
+ *
+ * Input: 4->2->1->3
+ * Output: 1->2->3->4
+ * Example 2:
+ *
+ * Input: -1->5->3->4->0
+ * Output: -1->0->3->4->5
+ */
 public class _0148SortList extends BaseLinkedList {
 
     private abstract static class Solution {
