1e78fc5bf9aacb78116cd045a33bf3d650f183e1
安明宇 <anmingyu@dhgate.com>
2020-07-17 19:01:27 +0800

    [Leetcode] 142 Linked List Cyle 2.
    description and reports.
    
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- LeetCode/src/_java/_0142LinkedListCycle2.java
+++ LeetCode/src/_java/_0142LinkedListCycle2.java
@@ -7,15 +7,35 @@ import base.BaseLinkedList;
 import base.util.ListNodeUtil;
 
 /**
- * Given a linked list, return the node where the cycle begins.
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
  * <p>
- * If there is no cycle, return null.
- * <p>
- * To represent a cycle in the given linked list,
- * we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
- * If pos is -1, then there is no cycle in the linked list.
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
  * <p>
  * Note: Do not modify the linked list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+ * <p>
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * <p>
+ * <p>
+ * Follow-up:
+ * Can you solve it without using extra space?
  */
 public class _0142LinkedListCycle2 extends BaseLinkedList {
 
