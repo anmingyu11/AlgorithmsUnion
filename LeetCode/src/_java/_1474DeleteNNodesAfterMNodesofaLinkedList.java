5af6f243a442fde76ecee936d4fa39cd635021e6
安明宇 <anmingyu@dhgate.com>
2020-08-20 10:17:42 +0800

    [Leetcode] 1474 delete N Nodes after m nodes of a linkedlist.
    Solutions and reports.
    
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

---
+++ LeetCode/src/_java/_1474DeleteNNodesAfterMNodesofaLinkedList.java
@@ -0,0 +1,81 @@
+package _java;
+
+import base.BaseLinkedList;
+
+/**
+ * Given the head of a linked list and two integers m and n.
+ * Traverse the linked list and remove some nodes in the following way:
+ * <p>
+ * Start with the head as the current node.
+ * Keep the first m nodes starting with the current node.
+ * Remove the next n nodes
+ * Keep repeating steps 2 and 3 until you reach the end of the list.
+ * Return the head of the modified list after removing the mentioned nodes.
+ * <p>
+ * Follow up question: How can you solve this problem by modifying the list in-place?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,4,5,6,7,8,9,10,11,12,13], m = 2, n = 3
+ * Output: [1,2,6,7,11,12]
+ * Explanation: Keep the first (m = 2) nodes starting from the head of the linked List  (1 ->2) show in black nodes.
+ * Delete the next (n = 3) nodes (3 -> 4 -> 5) show in read nodes.
+ * Continue with the same procedure until reaching the tail of the Linked List.
+ * Head of linked list after removing nodes is returned.
+ * Example 2:
+ * <p>
+ * Input: head = [1,2,3,4,5,6,7,8,9,10,11], m = 1, n = 3
+ * Output: [1,5,9]
+ * Explanation: Head of linked list after removing nodes is returned.
+ * Example 3:
+ * <p>
+ * Input: head = [1,2,3,4,5,6,7,8,9,10,11], m = 3, n = 1
+ * Output: [1,2,3,5,6,7,9,10,11]
+ * Example 4:
+ * <p>
+ * Input: head = [9,3,7,7,9,10,8,2], m = 1, n = 2
+ * Output: [9,7,8]
+ * <p>
+ * Constraints:
+ * <p>
+ * The given linked list will contain between 1 and 10^4 nodes.
+ * The value of each node in the linked list will be in the range [1, 10^6].
+ * 1 <= m,n <= 1000
+ */
+public class _1474DeleteNNodesAfterMNodesofaLinkedList extends BaseLinkedList {
+
+    private static abstract class Solution {
+        public abstract ListNode deleteNodes(ListNode head, int m, int n);
+    }
+
+    private static class Solution1 extends Solution {
+
+        public ListNode deleteNodes(ListNode head, int m, int n) {
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode slow = head, fast = slow;
+            while (slow != null) {
+                for (int i = m - 1; i > 0 && slow != null; --i, slow = slow.next) ;
+                if (slow == null) {
+                    break;
+                }
+                fast = slow;
+                for (int i = n; i >= 0 && fast != null; --i, fast = fast.next) ;
+                slow.next = fast;
+                slow = fast;
+            }
+            return dummy.next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution1();
+
+        ListNode l1 = generateASingleListNode(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13});
+        ListNode l2 = generateASingleListNode(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
+        ListNode res1 = s.deleteNodes(l1, 2, 3);
+        printSingleListNode(res1);
+
+    }
+}
