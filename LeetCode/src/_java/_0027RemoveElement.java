6cb06dd83c4eac64990f239416efac8f4fc343ba
安明宇 <anmingyu@dhgate.com>
2020-09-08 17:33:27 +0800

    [Leetcode] 27 Remove Element.
    Descriptions and Reports.
    
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- LeetCode/src/_java/_0027RemoveElement.java
+++ LeetCode/src/_java/_0027RemoveElement.java
@@ -2,6 +2,48 @@ package _java;
 
 import base.Base;
 
+/**
+ * Given an array nums and a value val,
+ * remove all instances of that value in-place and return the new length.
+ * <p>
+ * Do not allocate extra space for another array,
+ * you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * <p>
+ * Example 1:
+ * <p>
+ * Given nums = [3,2,2,3], val = 3,
+ * <p>
+ * Your function should return length = 2, with the first two elements of nums being 2.
+ * <p>
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ * <p>
+ * Given nums = [0,1,2,2,3,0,4,2], val = 2,
+ * <p>
+ * Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+ * <p>
+ * Note that the order of those five elements can be arbitrary.
+ * <p>
+ * It doesn't matter what values are set beyond the returned length.
+ * Clarification:
+ * <p>
+ * Confused why the returned value is an integer but your answer is an array?
+ * <p>
+ * Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+ * <p>
+ * Internally you can think of this:
+ * <p>
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeElement(nums, val);
+ * <p>
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ */
 public class _0027RemoveElement extends Base {
 
     private abstract static class Solution {
@@ -23,23 +65,21 @@ public class _0027RemoveElement extends Base {
         }
     }
 
-    // 6ms 这个思想很有意思,怎么想出来的呢.
+    // 6ms
+    // 思想类似于快速排序
     private static class Solution2 extends Solution {
 
         @Override
         int removeElement(int[] nums, int val) {
-            int n = nums.length;
-
             int i = 0;
-            while (i < n) {
+            for (int j = nums.length - 1; i <= j; ) {
                 if (nums[i] == val) {
-                    nums[i] = nums[n-- - 1];
+                    nums[i] = nums[j--];
                 } else {
                     ++i;
                 }
             }
-
-            return n;
+            return i;
         }
 
     }
